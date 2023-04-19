f955824660f7df4f6f748b60a177c3ad26ea0a00
Zakhy <lirchzakhar@gmail.com>
2023-04-19 11:39:09 +0700

    Fix queries for show_orders_per_shift and show_order functions.

--- orders/orders.py
+++ orders/orders.py
@@ -48,7 +48,7 @@ def create_order():
 
 @orders.route("/api-tort/work-shift/<shift_id>/orders")
 @auth.login_required(role=3)
-def show_orders_per_shift(shift_id):
+def show_orders_per_shift(shift_id): # TODO FIX QUERIES
     orders_per_shift = Orders.query.filter(Orders.shift_id == shift_id)
     current_shift = Shifts.query.filter(Shifts.id == shift_id).first_or_404()
     list_of_orders = []
@@ -73,41 +73,54 @@ def show_orders_per_shift(shift_id):
     }})
 
 
-@orders.route("api-cafe/order/<order_id>", methods=["GET"])
-@auth.login_required(role=[1, 2])
-def show_order(order_id):  # TODO test func
-    order = Orders.query(Orders).join(Users, Users.id == Orders.id).join(OrderStatuses)  # TODO test query
+@orders.route("/api-cafe/order/<order_id>", methods=["GET"])
+@auth.login_required(role=[1, 3])
+def show_order(order_id):
+    order = db.session.query(Orders, Users, OrderStatuses) \
+        .join(Users, Orders.user_id == Users.id) \
+        .join(OrderStatuses, OrderStatuses.id == Orders.status_id) \
+        .filter(Orders.id == order_id) \
+        .first()
     return jsonify({
-        "id": order.id,
-        "order_table": order.order_table,
-        "created_at": order.date_time,
-        "price": order.price,
-        "status": order.status_name,
-        "user": order.name,
-        "shift": order.shift_id
+        "id": order[0].id,
+        "order_table": order[0].order_table,
+        "created_at": order[0].date_time,
+        "price": order[0].price,
+        "status": order[2].status_name,
+        "user": order[1].name,
+        "shift": order[0].shift_id
     })
 
 
 @orders.route("/api-cafe/order/<order_id>/change-status", methods=["PATCH"])
-@auth.login_required(role=[1, 2])
-def change_status(order_id):  # TODO test this func
+@auth.login_required(role=[1, 3])
+def change_status(order_id):
     status = request.json["status"]
     status_id = 0
     if status == "canceled":
-        status_id = ...
-    elif status == "payed":
-        status_id = ...
+        status_id = 4
+    elif status == "done":
+        status_id = 1
 
-    order = Orders.query.filter(Orders.id == order_id)
+    order = db.session.query(Orders).filter(Orders.id == order_id).first()
     order.status_id = status_id
+    db.session.add(order)
+    db.session.commit()
     return jsonify({"data": f"status changed to {status}"})
 
 
 @orders.route("/api-cafe/order/<order_id>/position", methods=["POST"])
-@auth.login_required(role=[1, 2])
+@auth.login_required(role=[1, 3])
 def add_position(order_id):
-    menu_id = request.json["menu_id"]
-    count = request.json["count"]
+    menu_id = int(request.json["menu_id"])
+    quantity = int(request.json["count"])
 
-    order_items = OrderItems.query.filter(Orders.id == order_id)
-    # TODO complete func
+    order_items = OrderItems(
+        order_id=order_id, menu_item_id=menu_id, quantity=quantity
+    )
+    db.session.add(order_items)
+    db.session.commit()
+
+    return jsonify({
+        "data": "position was added"
+    })
